427c42a876239443758fe47dadf16e738510aebc
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-19 00:55:49 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+// A VARIABLE TO HOLD THE API URL
+const API_URL: string = "http://www.omdbapi.com/?apikey=86d3b8e1"
+
+// DECLARING A FUNCTION THAT RETURNS AN APP COMPONENT
+export default function App() {
+    return <h1>Hello World</h1>
+}
